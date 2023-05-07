ecc265704817f164f1cb4ec535feb1e6357b468f
Henna Singh <hennasingh04@gmail.com>
2023-05-07 18:07:50 +0100

    added Meme component with 2 inputs and 1 button and styled it.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
         <header className="header">
             <img className="logo" src={logo} />
             <h3 className='title'>Meme Generator</h3>
-            <h4 className='desc'>React Course - Project 3</h4>
+            <h4 className='header-desc'>React Course - Project 3</h4>
         </header>
         
     )
