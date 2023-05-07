f6c839947e55304418a8d9b6bfdd899d66a03dbf
Henna Singh <hennasingh04@gmail.com>
2023-05-07 16:51:26 +0100

    initial commit for a React  Meme Generator App.

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../imgs/troll-face.png'
+
+export default function Header() {
+
+    return (
+        <header className="header">
+            <img className="logo" src={logo} />
+            <h3 className='title'>Meme Generator</h3>
+            <h4 className='desc'>React Course - Project 3</h4>
+        </header>
+        
+    )
+
+}
