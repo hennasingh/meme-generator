7d0a7d40bd8bc00489166c32032575dab654ad63
Henna Singh <hennasingh04@gmail.com>
2023-05-19 07:42:01 +0100

    added memes data and meme component to show an image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import memes from '../memesData';
 
 export default function Meme() {
 
+    //const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText:"",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMeme] = React.useState(memes)
+
+    function getMemeImage() {
+        const memesData = memes.data.memes
+        const index = Math.floor(Math.random() * memesData.length)
+        const url = memesData[index].url
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage:url
+        }))
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text" 
                     placeholder='Top Text' 
@@ -13,10 +34,13 @@ export default function Meme() {
                     type="text" 
                     placeholder='Bottom Text' 
                     className='form-input'/>
-                <button className='form-button'>
-                    Get a new meme image 🖼
+                <button 
+                    className='form-button' 
+                    onClick={getMemeImage}>
+                Get a new meme image 🖼
                 </button>
-            </form> 
+            </div> 
+            <img src={meme.randomImage} className="meme-image"/>
         </main>
     )
 }
